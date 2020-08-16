86aac5e1db11e91870148f0e10821576167be59e
Sokolov Innokenty <im@qbbr.io>
2020-08-16 23:32:14 +0800

    use card for profile

--- components/Profile.js
+++ components/Profile.js
@@ -1,27 +1,29 @@
 export default {
     name: 'Profile',
     template: `
-        <div class="auth">
-            <h4 class="mb-4">Profile</h4>
-            <form @submit.prevent="update" :class="{ 'loading': loading }">
-                <div class="form-group">
-                    <label for="username" class="sr-only">Username</label>
-                    <input type="text" id="username" class="form-control" v-model="username" readonly>
-                </div>
-                <div class="form-group">
-                    <label for="token" class="sr-only">Token</label>
-                    <input type="text" id="token" class="form-control" v-model="token" readonly>
-                </div>
-                <div class="form-group">
-                    <label for="about" class="sr-only">About</label>
-                    <textarea id="about" class="form-control" v-model="about"></textarea>
-                </div>
-                <div class="form-group">
-                    <label for="createdAt" class="sr-only">Created at</label>
-                    <input type="text" id="createdAt" class="form-control" v-model="createdAt" readonly>
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">Update</button>
-            </form>
+        <div class="card bg-light mx-auto" style="max-width: 30rem;" :class="{ 'loading': loading }">
+            <div class="card-header">Profile</div>
+            <div class="card-body">
+                <form @submit.prevent="update" :class="{ 'loading': loading }">
+                    <div class="form-group">
+                        <label for="username" class="sr-only">Username</label>
+                        <input type="text" id="username" class="form-control" v-model="username" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="token" class="sr-only">Token</label>
+                        <input type="text" id="token" class="form-control" v-model="token" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="about" class="sr-only">About</label>
+                        <textarea id="about" class="form-control" v-model="about"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="createdAt" class="sr-only">Created at</label>
+                        <input type="text" id="createdAt" class="form-control" v-model="createdAt" readonly>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Update</button>
+                </form>
+            </div>
         </div>
     `,
     data() {
