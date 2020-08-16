1b215ab0e08750c1f5632f461a4aed6e84f387d0
Sokolov Innokenty <im@qbbr.io>
2020-08-16 22:46:53 +0800

    refactor paginator, now using query params

--- components/Index.js
+++ components/Index.js
@@ -2,14 +2,16 @@ export default {
     name: 'Index',
     template: `
         <div class="container" :class="{ 'loading': loading }">
+            <div v-if="posts.length === 0" style="height: 400px;"></div>
+        
             <div v-for="post in posts" :key="post.id">
                 <h2><a :href="'/post/' + post.slug">{{ post.title }}</a></h2>
                 <div class="mb-2">
                     <time :datetime="post.createdAt">{{ post.createdAt | formatDate }}</time>
                     by <b>{{ post.user.username }}</b>
                 </div>
-                <div class="">
-                    <a href="#" v-for="tag in post.tags" @click.prevent="filterByTag(tag)">{{ tag.name }}</a>
+                <div>
+                    <router-link :to="{ name: 'index', query: { tag: tag.name } }" v-for="tag in post.tags" :key="tag.name">{{ tag.name }}</router-link>
                 </div>
                 <p class="text-justify">{{ post.text }}</p>
             </div>
@@ -17,13 +19,13 @@ export default {
             <nav aria-label="Page navigation example" v-if="pages.length > 1">
                 <ul class="pagination justify-content-center">
                     <li class="page-item" :class="{ 'disabled': page === 1 }">
-                        <a href="#" class="page-link" @click.prevent="prevPage">Previous</a>
+                        <router-link :to="{ name: 'index', query: getParams(page - 1) }" class="page-link">Previous</router-link>
                     </li>
                     <li class="page-item" v-for="p in pages" :class="{ 'active': p === page }">
-                        <a href="#" class="page-link" @click.prevent="gotoPage(p)">{{ p }}</a>
+                        <router-link :to="{ name: 'index', query: getParams(p) }" class="page-link">{{ p }}</router-link>
                     </li>
                     <li class="page-item" :class="{ 'disabled': page === pages.length  }">
-                        <a href="#" class="page-link" @click.prevent="nextPage">Next</a>
+                        <router-link :to="{ name: 'index', query: getParams(page + 1) }" class="page-link">Next</router-link>
                     </li>
                 </ul>
             </nav>
@@ -33,58 +35,56 @@ export default {
         return {
             loading: true,
             posts: [],
-            page: 1,
-            pages: [],
-            tag: '',
-            query: ''
+            pages: []
         }
     },
     mounted() {
         this.getPosts();
-        this.$root.$on('search', (data) => {
-            this.query = data;
-            this.getPosts();
-        });
+    },
+    computed: {
+        page() {
+            return Number(this.$route.query.page) || 1;
+        },
+        tag() {
+            return this.$route.query.tag || '';
+        },
+        query() {
+            return this.$route.query.query || '';
+        }
+    },
+    watch: {
+       page: 'getPosts',
+       tag: 'getPosts',
+       query: 'getPosts'
     },
     methods: {
-        getPosts() {
-            this.loading = true;
-            const params = {
-                page: this.page
-            };
-
+        getParams(page) {
+            const params = {};
+            if (page !== 1) {
+                params.page = page;
+            }
             if (this.tag.length) {
                 params.tag = this.tag;
             }
-
             if (this.query.length) {
                 params.query = this.query;
             }
 
-            Vue.http.get('posts/', { params: params }).then(response => {
-                this.posts = response.data.results;
-                this.pages = [];
+            return params;
+        },
+        getPosts() {
+            this.loading = true;
+            Vue.http.get('posts/', { params: this.getParams(this.page) }).then(response => {
+                let pages = [];
                 for (let i = 1; i <= Math.ceil(response.data.total / 10); i++) {
-                    this.pages.push(i);
+                    pages.push(i);
                 }
+                this.pages = pages;
+                this.posts = response.data.results;
+                this.loading = false;
+            }, response => {
                 this.loading = false;
             });
-        },
-        filterByTag(tag) {
-            this.tag = tag.name;
-            this.getPosts();
-        },
-        gotoPage(page) {
-            this.page = page;
-            this.getPosts();
-        },
-        nextPage() {
-            this.page++;
-            this.getPosts();
-        },
-        prevPage() {
-            this.page--;
-            this.getPosts();
         }
     },
     filters: {
